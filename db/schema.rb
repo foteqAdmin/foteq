7c05ac026896ca3e0b3ac8a17b8518d868a40a72
Ruben O Chiavone <ruben_chiavone@hotmail.com>
2012-06-14 11:08:48 -0300

    Deleted files and seed updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120531142945) do
+ActiveRecord::Schema.define(:version => 20120604113747) do
+
+  create_table "data_files", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "participantes", :force => true do |t|
     t.string   "nome"
